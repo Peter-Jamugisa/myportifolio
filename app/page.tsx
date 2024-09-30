d4e59d1ae3b5043308b7c3b59bb8d2c3eabc583e
Peter-Jamugisa <105854604+Peter-Jamugisa@users.noreply.github.com>
2024-09-30 05:39:01 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,17 @@ import Projects from "@/components/Projects/Projects";
 import Services from "@/components/Services/Services";
 import Footer from "@/components/ui/Footer";
 import NavBar from "@/components/ui/Navbar";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main >
+            <Head>
+        <title>Jamugisa Peter Paul - Portfolio</title>
+        <meta name="description" content="Portfolio of Jamugisa Peter Paul - frontend developer" />
+        <meta name="keywords" content="Jamugisa Peter Paul,Jamugisa,Jamugisa Peter,Jamugisa Paul, portfolio, frontend developer" />
+      </Head>
       <NavBar />
 
       <div>
